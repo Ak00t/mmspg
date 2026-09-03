78f1ddf312160be14f1ac4c0a72ea4a832dabf58
Ak00t <aungkthu01@gmail.com>
2026-09-03 09:43:50 +0630

    start

---
+++ src/main/java/com/ojt_22/mmspg/MmspgApplication.java
@@ -0,0 +1,13 @@
+package com.ojt_22.mmspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmspgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmspgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ojt_22/mmspg/MmspgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojt_22.mmspg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmspgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
