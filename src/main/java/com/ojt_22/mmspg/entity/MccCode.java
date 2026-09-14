2c9626efa45cf34c7a322b2d54d683f70b5c9665
winwin555620-sys <winwin555620@gmail.com>
2026-09-14 13:24:34 +0630

    insert of @UpdateTimestamp

--- src/main/java/com/ojt_22/mmspg/entity/MccCode.java
+++ src/main/java/com/ojt_22/mmspg/entity/MccCode.java
@@ -3,6 +3,7 @@ package com.ojt_22.mmspg.entity;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -44,6 +45,7 @@ public class MccCode {
 	@Column(name = "created_at", nullable = false)
 	private LocalDateTime createdAt;
 
+	@UpdateTimestamp
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 
