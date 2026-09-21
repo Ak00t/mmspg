c39d8d581ba328413ea2f82992032bc59e77d7a5
Ak00t <aungkthu01@gmail.com>
2026-09-21 10:31:54 +0630

    changed interface impl

--- src/main/java/com/ojt_22/mmspg/controller/admin/FeeController.java
+++ src/main/java/com/ojt_22/mmspg/controller/admin/FeeController.java
@@ -18,45 +18,39 @@ import com.ojt_22.mmspg.dto.FeeConfigRequest;
 import com.ojt_22.mmspg.dto.FeeConfigResponse;
 import com.ojt_22.mmspg.service.FeeService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/admin/fees")
+@RequiredArgsConstructor
 public class FeeController {
-	
+
 	private final FeeService feeService;
-	
-	public FeeController(FeeService feeService) {
-		this.feeService = feeService;
-	}
-	
+
 	@PostMapping
-	public ResponseEntity<FeeConfigResponse> createFeeConfig(@RequestBody FeeConfigRequest request){
-		
+	public ResponseEntity<FeeConfigResponse> createFeeConfig(@RequestBody FeeConfigRequest request) {
+
 		FeeConfigResponse response = feeService.createFeeConfig(request);
-		
-		return ResponseEntity
-				.status(HttpStatus.CREATED)
+
+		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(response);
 	}
-	
+
 	@PutMapping("/{feeId}")
-	public ResponseEntity<FeeConfigResponse> updateFeeConfig(
-	        @PathVariable UUID feeId,
-	        @RequestBody FeeConfigRequest request) {
+	public ResponseEntity<FeeConfigResponse> updateFeeConfig(@PathVariable UUID feeId,
+			@RequestBody FeeConfigRequest request) {
 
-	    FeeConfigResponse response =
-	            feeService.updateFeeConfig(feeId, request);
+		FeeConfigResponse response = feeService.updateFeeConfig(feeId, request);
 
-	    return ResponseEntity.ok(response);
+		return ResponseEntity.ok(response);
 	}
-	
+
 	@PostMapping("/calculate")
-	public ResponseEntity<FeeCalculationResponse> calculateFee(
-	        @RequestParam UUID feeId,
-	        @RequestParam BigDecimal amount) {
+	public ResponseEntity<FeeCalculationResponse> calculateFee(@RequestParam UUID feeId,
+			@RequestParam BigDecimal amount) {
 
-	    FeeCalculationResponse response =
-	            feeService.calculateFee(feeId, amount);
+		FeeCalculationResponse response = feeService.calculateFee(feeId, amount);
 
-	    return ResponseEntity.ok(response);
+		return ResponseEntity.ok(response);
 	}
 }
